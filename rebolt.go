b7649a38166f42d31e86582b86c9f10bdf49daca
John Hartsfield <john@unilink.local>
2024-12-24 20:47:36 -0500

    updates

--- rebolt.go
+++ rebolt.go
@@ -50,6 +50,7 @@ func startServices() {
 			com := "go build -o " + domain
 			fmt.Println("Running: ", com)
 			fmt.Println(localCommand(strings.Split(com, " ")))
+			fmt.Println(localCommand(strings.Split("mv "+domain+" ~/bin", " ")))
 			fmt.Println(localCommand([]string{domain}))
 		}
 	}
