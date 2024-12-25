975ec3abe67dccc49b2441fc379c6b6db24fad3b
John Hartsfield <john@unilink.local>
2024-12-24 21:00:26 -0500

    updates

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ func main() {
 	defer f.Close()
 	log.SetOutput(f)
 
-	makeUDP()
+	// makeUDP()
 
 	insecure := newServerConf(httpPort, http.HandlerFunc(forwardHTTP))
 	secure := newServerConf(tlsPort, http.HandlerFunc(forwardTLS))
