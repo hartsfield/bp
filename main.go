25cad256fc7412aed203e39806c079f9d47d01c1
John Hartsfield <john@unilink.local>
2024-12-24 22:44:43 -0500

    updates

--- main.go
+++ main.go
@@ -40,11 +40,11 @@ func main() {
 		if !strings.Contains(s, "www.") {
 			cdir := "/home/john/live/" + s + "/"
 			fmt.Println("  ->", v.App.Port, s)
-			com := strings.Split("go build -C"+cdir+" -o "+s, " ")
+			com := strings.Split("go build -C "+cdir+" -o "+s, " ")
 			fmt.Println(com)
 			fmt.Println(localCommand(com, false, cdir))
-			fmt.Println(localCommand([]string{"mv", "/home/john/live/" + s + "/" + s, "/home/john/bin/"}, false, cdir))
-			fmt.Println(localCommand([]string{"cd", "/home/john/live/" + s}, false, cdir))
+			// fmt.Println(localCommand([]string{"mv", "/home/john/live/" + s + "/" + s, "/home/john/bin/"}, false, cdir))
+			// fmt.Println(localCommand([]string{"cd", "/home/john/live/" + s}, false, cdir))
 			fmt.Println(localCommand([]string{s}, true, cdir))
 		}
 	}
